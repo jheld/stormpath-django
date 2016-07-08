6d4e9412ee997df67daa1dc5fbf283f87cf6c28c
Jason Held <jasonsheld@gmail.com>
2016-07-08 15:06:38 -0400

    1062 version

--- django_stormpath/__init__.py
+++ django_stormpath/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('1', '0', '6', '1')
+__version_info__ = ('1', '0', '6', '2')
 __version__ = '.'.join(__version_info__)
 __short_version__ = '.'.join(__version_info__)
 __author__ = 'Stormpath, Inc.'
